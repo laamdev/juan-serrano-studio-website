be78215ba7ee491f5814c6d51fd29cab8db8e28f
Luis Anaya <brothersailorwebdev@gmail.com>
2022-12-16 19:39:34 +0100

    modified styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,14 +11,14 @@
 
 <div class="mx-auto grid grid-cols-1 gap-y-10 py-5 lg:grid-cols-2 lg:py-0">
 	<div class="col-span-1 flex flex-col place-content-center place-items-center">
-		<h1 class="font-serif text-7xl lg:text-9xl">
-			<span class="block font-bold text-neutral-900">Juan</span>
-			<span class="ml-20 block italic text-blue-700">Serrano</span>
-			<span class="ml-40 block font-bold text-neutral-900">Studio</span>
-		</h1>
-		<h2 class="mt-2.5 text-center text-lg text-neutral-700 lg:mt-5 lg:text-2xl">
-			Muebles Artesanos
-		</h2>
+		<div>
+			<h1 class="font-serif text-8xl lg:text-9xl">
+				<span class="block font-bold text-neutral-900">Juan</span>
+				<span class="block italic text-blue-700">Serrano</span>
+				<span class="block font-bold text-neutral-900">Studio</span>
+			</h1>
+			<h2 class="mt-2.5 text-lg text-neutral-700 lg:mt-5 lg:text-2xl">Muebles Artesanos</h2>
+		</div>
 	</div>
 	<ul
 		class="col-span-1 flex h-full flex-col gap-y-10 gap-x-5 overflow-y-scroll px-2.5 scrollbar-thin scrollbar-thumb-blue-700 lg:max-h-screen lg:px-0"
